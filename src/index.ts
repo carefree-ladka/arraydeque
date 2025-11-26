fef3c306e8e0cc585ea34ac091c08a25c81e4980
Pawan Kumar <pawan.kumar@compassdigital.io>
2025-11-26 22:42:48 +0530

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
-export { default as Deque } from "./Deque";
-export { default as PriorityQueue } from "./PriorityQueue";
+export { default as Deque } from "./Deque.js";
+export { default as PriorityQueue } from "./PriorityQueue.js";
+export type { Comparator, HeapType, PriorityQueueOptions } from "./PriorityQueue.js";
